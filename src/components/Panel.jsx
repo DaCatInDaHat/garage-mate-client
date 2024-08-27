104c45b047e12e03b9658fb7ed8804f8a6b198bd
DaCatInDaHat <volodymyrkuznetsov87@gmail.com>
2024-08-27 13:47:42 +0300

    change path to create component in panel component

--- src/components/Panel.jsx
+++ src/components/Panel.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Button } from '@mui/material'
+import { useNavigate } from 'react-router-dom'
 
 const Panel = () => {
+    const navigateTo = useNavigate()
+
     return (
         <Button
             variant='outlined'
             sx={{ m: 1 }}
-            href='./create'
+            onClick={navigateTo('/create')}
         >
             Create
         </Button>
